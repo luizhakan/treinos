e001899cc516b29a4e51f8893519731d5f4fdcef
luizhakan <uefibiosxx@gmail.com>
2022-04-15 23:33:11 -0300

    15/05/2022

--- consumindo-api/fetch-api02/script.js
+++ consumindo-api/fetch-api02/script.js
@@ -1,36 +1,38 @@
 // MÉTODO POST é um upload de dados para um servidor
 let surpresa = () => {
-    fetch("https://jsonplaceholder.typicode.com/todos", {method : "POST", headers: {"Content-Type": "application/json"},body: JSON.stringify({title: "LUIZ HAKAN É O MELHOR DE TODOS, RECEBA!"})}).then((response) => {
-        response.json().then((data) => {
-            alert(data.title);
-            const li = document.querySelector('#lista-preenchida')
-            li.innerHTML += `
+  fetch("https://jsonplaceholder.typicode.com/todos", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ title: "LUIZ HAKAN É O MELHOR DE TODOS, RECEBA!" }),
+  }).then((response) => {
+    response.json().then((data) => {
+      alert(data.title);
+      const li = document.querySelector("#lista-preenchida");
+      li.innerHTML += `
                 <ul>
                 <li>${data.id}</li>
                 <li>${data.title}</li>
                 </ul>
                 <br>
-            `
-        })
-    })
-}
+            `;
+    });
+  });
+};
 
 // Por padrão isso aqui é um MÉTODO GET, é um download de dados de um servidor
 let fetchApiData = () => {
   fetch("https://jsonplaceholder.typicode.com/todos").then((response) => {
-    response.json().then((data)=> {
-        const li = document.querySelector('#lista-preenchida')
-        data.map((item) => {
-            li.innerHTML += `
+    response.json().then((data) => {
+      const li = document.querySelector("#lista-preenchida");
+      data.map((item) => {
+        li.innerHTML += `
             <ul>
             <li>${item.id}</li>
             <td>${item.title}</td>
             </ul>
             <br>
-        `
-        })
-
-        ;
+        `;
+      });
     });
   });
 };
