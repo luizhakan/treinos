42d32dbdd1095163d16b8f9257aa9f92474507a2
luizhakan <uefibiosxx@gmail.com>
2022-04-17 21:56:59 -0300

    17/04/2022

--- consumindo-api/fetch-api04/script.js
+++ consumindo-api/fetch-api04/script.js
@@ -3,7 +3,6 @@ function getApi(){
         response.json().then((data) => {
             data.map((itens) => {
                 let ul = document.querySelector('.lista')
-                console.log(itens)
                 ul.innerText += `
                                 ID : ${itens.id}
                                 userId : ${itens.userId}
